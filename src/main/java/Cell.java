72e33ee609eca90c7a9d5df4fcb3fff386702291
Esteban Bravo <jorgeesteban.bravosanchez@external.engie.com>
2023-03-14 22:26:28 +0100

    G:Any live cell with fewer than two live neighbours dies, as if by underpopulation

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Cell {
+public class Cell {
+
+    private static final int ALIVE = 1;
+    private static final int DEAD = 0;
+
+    private int state;
+    private int numberOfAliveNeighbours;
+
+    private Cell() {
+    }
+
+    public Cell(final int state, final int numberOfAliveNeighbours) {
+        this.state = state;
+        this.numberOfAliveNeighbours = numberOfAliveNeighbours;
+    }
+
+    public boolean isAlive() {
+        return ALIVE == state;
+    }
+
+    public int getNextState() {
+        if (isAlive() && numberOfAliveNeighbours < 2) {
+            return DEAD;
+        }
+
+        return ALIVE;
+
+    }
 }
