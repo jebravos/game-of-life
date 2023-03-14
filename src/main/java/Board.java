76b604e7b373ecfd3a0a74845f917e03913c0d0b
Esteban Bravo <jorgeesteban.bravosanchez@external.engie.com>
2023-03-14 20:26:45 +0100

    R: Given a board with only one cell alive when a tick happens then a dead board is produced

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,15 +1,13 @@
 public class Board {
 
+    private int[][] cells;
 
     public Board(final int[][] cells) {
+        this.cells = cells;
     }
 
     public int[][] tick(){
-        return new int[][]{
-                {0,0,0},
-                {0,0,0},
-                {0,0,0}
-        };
+        return this.cells;
     }
 
 }
--- src/test/java/GameOfLifeTests.java
+++ src/test/java/GameOfLifeTests.java
@@ -10,14 +10,28 @@ class GameOfLifeTests {
             {0,0,0},
             {0,0,0}
     };
+    public static final int[][] ONE_ALIVE = {
+            {0,0,0},
+            {0,1,0},
+            {0,0,0}
+    };
 
     @Test
     @DisplayName("A board full of dead cells must remain dead after a tick")
-    void deadBoardRemainsDead(){
+    void returnsDeadBoardWhenAllCellsAreDead(){
         // Given
         Board allCellsDeadBoard = new Board(DEAD_BOARD);
 
         assertThat(allCellsDeadBoard.tick()).isEqualTo(DEAD_BOARD);
+    }
+
+    @Test
+    @DisplayName("A cell without neighbours will die of underpopulation producing a dead board after a tick")
+    void returnsDeadBoardWhenOnlyOneCellIsAlive(){
+        // Given
+        Board oneAliveBoard = new Board(ONE_ALIVE);
+
+        assertThat(oneAliveBoard.tick()).isEqualTo(DEAD_BOARD);
     }
 
 }
